3dfe4b8bf0b3d1731557e92be0e4af0f1f9661b8
Lie Zeilstra <lzeilstra@icloud.com>
2017-11-11 23:02:40 +0100

    set cookie max-age in msecs

--- doc/import.sh
+++ doc/import.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mongoimport --host localhost --port 27017 -u 'tracker-user' -p 'welcome1' --authenticationDatabase admin --db tracker --collection locations --file ./data.json --jsonArray
+mongoimport --host localhost --port 27017 -u 'tracker-user' -p 'welcome1' --authenticationDatabase admin --db tracker --collection locations --upsert --file ./data.json --jsonArray
